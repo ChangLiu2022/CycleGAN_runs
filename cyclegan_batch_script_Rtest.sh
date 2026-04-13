7b31ec1afd375cc6e173ceb828c20da089609dcc
Chang Liu <eddyliu@gl-login6.arc-ts.umich.edu>
2026-04-13 14:51:01 -0400

    data

--- cyclegan_batch_script_Rtest.sh
+++ cyclegan_batch_script_Rtest.sh
@@ -11,7 +11,7 @@
 #SBATCH --gpus=a40:1                # number of gpus per node
 #SBATCH --cpus-per-gpu=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-gpu=32G     # memory per gpu- allocate as needed
-#SBATCH --time=120:00:00         # total run time limit (HH:MM:SS)
+#SBATCH --time=1:00:00         # total run time limit (HH:MM:SS)
 
 # Initialize conda for the shell
 eval "$(conda shell.bash hook)"
--- cyclegan_python_caller_Rtest.sh
+++ cyclegan_python_caller_Rtest.sh
@@ -4,6 +4,6 @@
 
 echo "🚀 Starting CycleGAN Model Training and Evaluation (Rtest)..."
 
-python cgan_longterm_stab_compare.py --config RR_CGAN_SAVE_Rtest --day0s "20200127,20210628"
+python cgan_longterm_stab_compare.py --config RR_CGAN_SAVE_Rtest --day0s "20230621,20230622"
 
 echo "✅ Training and Evaluation completed!"
