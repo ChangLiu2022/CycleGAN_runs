05614c7b2417beeb82993d1c79d2de2050fff353
ChangLiu <98478845+ChangLiu2022@users.noreply.github.com>
2026-04-16 16:02:38 -0400

    lstm nn job

---
+++ cyclegan_python_caller_LSTM_NN.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script to call the run-file for cycleGAN training
+
+echo "🚀 Starting CycleGAN Model Training and Evaluation..."
+
+python LSTM_LS_LINK.py --config LSTM_NN_LM
+
+
+echo "✅ Training and Evaluation completed!"
